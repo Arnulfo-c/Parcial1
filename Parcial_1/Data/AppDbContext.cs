13ecb10587f16b92ed1b97f59b0265a0d858f1e4
Arnulfo Cruz <ArnulfoVelasques@outlook.com>
2025-03-10 22:04:47 -0600

    Agregar archivos de proyecto.

---
+++ Parcial_1/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial1.Models;
+
+namespace Parcial1.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        public DbSet<Carreras> Carreras { get; set; }
+    }
+}
---
+++ Parcial_1/Models/Carreras.cs
@@ -0,0 +1,11 @@
+﻿namespace Parcial1.Models
+{
+    public class Carreras
+    {
+        public int Id { get; set; }
+        public required string Nombre { get; set; }
+        public required string Rector { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public bool Activo { get; set; }
+    }
+}
